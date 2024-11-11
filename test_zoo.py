4a908988654c0569c48a41e00d8afe1f7c586a10
dewmsr <kemisara.ana@student.mahidol.edu>
2024-11-11 04:59:11 +0000

    6687074 Kemisara Zoo

--- test_zoo.py
+++ test_zoo.py
@@ -7,7 +7,14 @@ class TestZoo(unittest.TestCase):
 
     def test_child_ticket_price(self):
         self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+    def test_baby_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), 0)
+    def test_teenager_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(14), 100)
+    def test_adult_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(23), 150)
+    def test_elderly_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(127), 100)   
     # Add your additional test cases here.
 
 if __name__ == '__main__':
